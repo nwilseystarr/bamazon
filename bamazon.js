98ceacdfbde114324c1b22677f99082ffa2fccda
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-02-28 07:32:54 -0600

    Min requirements complete, need to change password so it is blank rn

--- bamazon.js
+++ bamazon.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const chalk = require('chalk');
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "4eKGzay9!",
+    password: "",
     database: "bamazon"
 });
 
@@ -23,7 +24,7 @@ function displayAllItems() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw (err);
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | ʛ" + res[i].price);
+            console.log(chalk.green(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | ʛ" + res[i].price));
         }
         askUserBuy();
     })
@@ -45,18 +46,18 @@ function askUserBuy() {
         connection.query("SELECT * FROM products WHERE ?", { item_id: answer.likeToBuy }, function (err, res) {
             if (err) throw (err);
             if (answer.quantity <= res[0].stock_quantify) {
-                console.log("Placing order! \n");
+                console.log(chalk.blue("Placing order! \n"));
                 updateDB(answer.likeToBuy, answer.quantity, res[0].stock_quantify, res[0].price);
 
             } else {
-                console.log("Sorry, out of stock! \n")
+                console.log(chalk.blue("Sorry, out of stock! \n"));
             };
         })
     })
 }
 
 function updateDB(id, quantityA, currentQuantity, price) {
-    console.log("Updating quantity \n");
+    console.log(chalk.italic("Updating quantity \n"));
 
     connection.query("UPDATE products SET ? WHERE ?",
     [
@@ -69,7 +70,7 @@ function updateDB(id, quantityA, currentQuantity, price) {
     ],
         function (err, res) {
             if (err) throw (err);
-            console.log(res.affectedRows + " products updated \n");
+            console.log(chalk.bgGreen(res.affectedRows + " products updated \n"));
         }
     )
     console.log("Your total price is ʛ" + price + "\n");
